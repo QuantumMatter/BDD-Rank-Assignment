dd1af217f810a60b45319454e1faea06bba16512
QuantumMatter <kopala.david@gmail.com>
2025-09-09 19:28:25 -0400

    It works!

--- src/rank_order_assignment/real_hungarian_2.py
+++ src/rank_order_assignment/real_hungarian_2.py
@@ -1,51 +1,76 @@
 import numpy as np
 from collections import deque
 
-def adjacency_from_edges(nrow, ncol, edges):
-    adj = np.zeros((nrow, ncol))
-    for (i,j) in edges:
-        adj[i,j] = 1
-    return adj
-
 # This is done by starting at an unmatched node in S, and trying to
 # find a node in T that is unmatched, via a breadth first search
 # We continue queuing nodes from those reachable from T if T is
 # already in the tree
 def find_augmenting_path(adj, matching):
-    parent = {}
+    n = adj.shape[0]
 
     # First, find an unmatched node to start the search from
-    # Matches are edges that start in T and go to S, so they
-    # will be elements in our `tree` that have a value -1
-    unmatched_rows_mask = np.all(adj == 0, axis=1)
-    unmatched_rows = np.where(unmatched_rows_mask)[0]
+    unmatched_rows = [r for r in range(n) if r not in matching]
 
+    parent_row = -np.ones(n, dtype=int)
+    parent_col = -np.ones(n, dtype=int)
+    visited_rows = np.zeros(n, dtype=bool)
+    visited_cols = np.zeros(n, dtype=bool)
     queue = deque(unmatched_rows)
-    visited = set(unmatched_rows)
+
+    for row in unmatched_rows:
+        visited_rows[row] = True
+
+    augmenting_col = -1
 
     while len(queue) > 0:
-        row = queue.pop()
+        row = queue.popleft()
+        # print(f'Exploring row { row }')
 
         neighbor_columns = np.where(adj[row,:] == 1)[0]
         for col in neighbor_columns:
-            if matching[col] is not None:
+            if visited_cols[col]: continue
+
+            visited_cols[col] = True
+            parent_row[col] = row
+
+            if matching[col] != -1:
                 # The node has already been matched
                 # Queue its matched node in S
-                queue.append(matching[col])
-                visited.add(matching[col])
-                parent[matching[col]] = row
+                matched_row = matching[col]
+                # print(f'Row { row } has neighbor column { col } that is already matched to { matched_row }')
+                queue.append(matched_row)
+                visited_rows[matched_row] = True
+                parent_col[matched_row] = col
             else:
-                # The node has not been matched, so we've
-                # found an augmenting path!
-                # Reconstruct the path, and return it to the caller
+                # print(f'Row { row } has an augmenting path to column { col }')
+                augmenting_col = col
+                break
+                
+        if augmenting_col != -1:
+            break
 
-                # Add the current edge, from S to T
-                # Then, find the edge that went from T to S prior
-                path = [(row, col)]
-                path_row = row
-                while path_row != None:
-                    # Find the edge that goes from T to path_row in S
+    if augmenting_col == -1:
+        return None, visited_rows, visited_cols
+    
 
+    # The node has not been matched, so we've
+    # found an augmenting path!
+    # Reconstruct the path, and return it to the caller
+
+    path = []
+    curr_col = augmenting_col
+    while curr_col != -1:
+        curr_row = parent_row[curr_col]
+        path.append((curr_row, curr_col))
+
+        # Move to the previous row in the path
+        if curr_row in unmatched_rows:
+            break
+        
+        curr_col = parent_col[curr_row]
+
+    path.reverse()
+    return path, visited_rows, visited_cols
 
 
 def hungarian(cost_matrix):
@@ -54,33 +79,105 @@ def hungarian(cost_matrix):
     # If more "doctors" are needed, then make new rows of the max value
     [nrow, ncol] = cost_matrix.shape
     while nrow < ncol:
-        cost_matrix = np.vstack((cost_matrix, np.zeros(ncol)))
+        cost_matrix = np.vstack((cost_matrix, np.full(ncol, 1e10)))
         nrow += 1
     while ncol < nrow:
         # Need more positions
-        cost_matrix = np.column_stack((cost_matrix, np.zeros(nrow)))
+        cost_matrix = np.column_stack((cost_matrix, np.full(nrow, 1e10)))
         ncol += 1
 
     ## Keep track of the elements of our solution
     # Instantiate our potential
-    # y_row = np.zeros(nrow)
-    # y_col = np.zeros(ncol)
     y_row = cost_matrix.min(axis=1)
-    y_col = np.zeros(ncol)
+    y_col = np.zeros(ncol, dtype=int)
     # Instantiate the matching
     # Essentially a collection of edges from T to S,
     # where col is the ID of an elemnt in T, and its
     # value will the be ID of the element its matched to in S
-    matching = { col: None for col in range(ncol) }
+    # matching[j]=i  -->  row i is matched with column j
+    matching = -np.ones(ncol, dtype=int)
 
     # While we don't have a complete matching, try to iterate the algorithm
-    while any(map(lambda value: value is None, matching.values)):
+    while np.any(matching == -1):
+        # print(matching)
+        adj = np.zeros((nrow, ncol), dtype=int)
+        for i in range(nrow):
+            for j in range(ncol):
+                # Assume the bipartite graph is dense, and all students rank all programs
+                weight = cost_matrix[i,j]
+                if weight == y_row[i] + y_col[j]:
+                    adj[i,j] = 1
+
         # First, try to find an augmenting path
-        augmenting_path = find_augmenting_path()
+        augmenting_path, visited_rows, visited_cols = find_augmenting_path(adj, matching)
 
         if augmenting_path is not None:
             # We need to invert the matchings along the new path
-            pass
+            # The path should start and end with the "unmatched" edges
+            # Start traversing at the newly discovered leaf
+            for (i,j) in augmenting_path:
+                matching[j] = i
+
         else:
             # We need to update the dual
-            pass
+            # Find the minimum value to adjust the potential by,
+            # such that another tight edge is created between S and T
+            # and therefore a new augmenting path can be found
+            # This edge will exist between a visited row, and unvisited column
+            min_delta = 1e10
+            for i in range(nrow):
+                if not visited_rows[i]: continue
+                for j in range(ncol):
+                    if visited_cols[j]: continue
+                    slack = cost_matrix[i,j] - (y_row[i] + y_col[j])
+                    min_delta = min(min_delta, slack)
+
+            for i in range(nrow):
+                if not visited_rows[i]: continue
+                y_row[i] += min_delta
+            
+            for j in range(ncol):
+                if not visited_cols[j]: continue
+                y_col[j] -= min_delta
+
+    # We have our matching!
+    # print(matching)
+    return matching, np.sum(y_col) + np.sum(y_row)
+
+
+if __name__ == '__main__':
+    sample_costs = np.array([
+        [8, 4, 7],
+        [5, 2, 3],
+        [9, 6, 7],
+        [9, 4, 8]
+    ], dtype=int)
+
+    print(hungarian(sample_costs))
+
+    from scipy.optimize import linear_sum_assignment
+
+    rng = np.random.default_rng(seed=303)
+
+    for n in range(10, 100):
+        nrow = rng.integers(3, n+1)
+        ncol = n - nrow
+
+        cost = rng.integers(0, ncol, (nrow, ncol))
+
+        if (n % 10) == 0:
+            print(f"TEST STEP n={n} =======")
+            print(cost)
+
+        msol, _ = hungarian(cost)
+        mcost = 0
+        for (j,i) in enumerate(msol):
+            if i >= nrow: continue # Padding
+            if j >= ncol: continue # Padding
+            mcost += cost[i,j]
+
+        srow, scol = linear_sum_assignment(cost)
+        scost = cost[srow, scol].sum()
+
+        assert mcost == scost, "Solution doesn't match reference!"
+        print(f'Passed n={n}')
